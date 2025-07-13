86337af3d7cb7954da7b80976993c6b36ef84eaf
TejasTadikonda04 <tejastk04@hotmail.com>
2025-07-13 16:35:17 -0500

    delete 2009 download

--- src/scripts/read_pbp_2009.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from pathlib import Path
-
-parquet_path = Path("data/lake/pbp/season=2009/pbp_2009.parquet")
-
-df = pd.read_parquet(parquet_path)
-
-print(df.shape)
-print(df.head(3))
